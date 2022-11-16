ed3663b085901f40f06d2ee288f7088d1273c4f8
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:06:05 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c
    Sync to source repo @bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.dataTables.js
+++ js/fixedColumns.dataTables.js
@@ -28,10 +28,9 @@
 				require('datatables.net-dt')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.FixedColumns ) {
 				require('datatables.net-fixedcolumns')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/fixedColumns.dataTables.mjs
+++ js/fixedColumns.dataTables.mjs
@@ -4,7 +4,7 @@
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-dt';
-import 'datatables.net-fixedcolumns';
+import FixedColumns from 'datatables.net-fixedcolumns';
 
 
 
