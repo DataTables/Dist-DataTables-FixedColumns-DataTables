05ce51f02cbc9abb503a94e4b26595ce1cf4c07b
Allan Jardine <allan@datatables.net>
2026-05-07 15:35:34 +0000

    Updating package for DataTables 3

--- js/fixedColumns.dataTables.js
+++ js/fixedColumns.dataTables.js
@@ -1,3 +1,52 @@
-/*! DataTables integration for DataTables' FixedColumns
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedColumns default styling 6.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
+
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-dt', 'datatables.net-fixedcolumns'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-dt')(root);
+			}
+
+			if (! window.DataTable.FixedColumns) {
+				require('datatables.net-fixedcolumns')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+
+return DataTable;
+}));
