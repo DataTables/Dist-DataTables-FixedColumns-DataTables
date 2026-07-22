c4ed249f6e76e53c3e4399c7c6e51cbe14eda1c3
dtbuild <dtbuild@rigil>
2026-07-22 14:30:27 +0000

    8049aa7a424619cd089c370ea54bf8e892c03407 Release 6.0.0 ready
    Sync to source repo @8049aa7a424619cd089c370ea54bf8e892c03407

--- js/fixedColumns.dataTables.js
+++ js/fixedColumns.dataTables.js
@@ -1,4 +1,4 @@
-/*! FixedColumns default styling 6.0.0-beta.1 for DataTables
+/*! FixedColumns default styling 6.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
--- js/fixedColumns.dataTables.min.mjs
+++ js/fixedColumns.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! FixedColumns default styling 6.0.0-beta.1 for DataTables
+/*! FixedColumns default styling 6.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
